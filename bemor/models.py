395595d78985c7f49ef94434063178c2a15eb710
Saidorifkhuja Yuldoshkhujaev <yoldoshxojayevsaidorifxoja@gmail.com>
2025-03-10 14:01:25 +0500

    urologiya

--- bemor/models.py
+++ bemor/models.py
@@ -1,20 +1,26 @@
 from django.db import models
 from account.models import *
 import uuid
+from django.utils import timezone
 from shifokor.models import Shifokor
 
 
 class Bemor(models.Model):
     uid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
     name = models.CharField(max_length=1000)
+    sana = models.DateTimeField(default=timezone.now)
+    tugilgan = models.DateTimeField(default=timezone.now)
+    manzil = models.TextField()
     kasallik = models.TextField()
+    anamnesis_m = models.TextField()
+    anamnesis_v = models.TextField()
+    praesens = models.TextField()
+    tekshiruv = models.TextField()
     tashxis = models.TextField()
+    tavsiya = models.TextField()
     doctor = models.CharField(max_length=500)
-    # doctor = models.ForeignKey(Shifokor, on_delete=models.CASCADE, null=True, blank=True)
-
+    mudir = models.CharField(max_length=500)
     def __str__(self):
         return self.name
-
-
 
 
--- bemor/views.py
+++ bemor/views.py
@@ -1,10 +1,16 @@
+from tkinter import Frame
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.units import cm
+from reportlab.lib.enums import TA_CENTER
 from rest_framework.permissions import IsAdminUser
 from rest_framework.parsers import MultiPartParser, FormParser
-from django.http import HttpResponse
 from rest_framework import generics, filters
-from docx import Document
 from .models import Bemor
 from .serializers import BemorSerializer
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.platypus import Paragraph
+from django.http import HttpResponse
 
 
 class BemorCreateView(generics.CreateAPIView):
@@ -17,29 +23,7 @@ class BemorCreateView(generics.CreateAPIView):
         response = super().create(request, *args, **kwargs)
         return response
 
-class BemorRetrieveView(generics.RetrieveAPIView):
-    queryset = Bemor.objects.all()
-    serializer_class = BemorSerializer
-    lookup_field = 'uid'
 
-    def get(self, request, *args, **kwargs):
-        bemor = self.get_object()  # Retrieve the Bemor object
-
-
-        doc = Document()
-        doc.add_heading('Bemor Ma\'lumotlari', level=1)
-
-        # doc.add_paragraph(f"ID: {bemor.uid}")
-        doc.add_paragraph(f"Ism: {bemor.name}")
-        doc.add_paragraph(f"Kasallik: {bemor.kasallik}")
-        doc.add_paragraph(f"Tashxis: {bemor.tashxis}")
-        doc.add_paragraph(f"Shifokor: {bemor.doctor}")
-
-        # Save to a temporary file
-        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
-        response['Content-Disposition'] = f'attachment; filename=Bemor_{bemor.uid}.docx'
-        doc.save(response)
-        return response
 
 class BemorUpdateView(generics.UpdateAPIView):
     queryset = Bemor.objects.all()
@@ -61,34 +45,6 @@ class BemorListView(generics.ListAPIView):
 
 
 
-class BemorDocxDownloadView(generics.RetrieveAPIView):
-    queryset = Bemor.objects.all()
-    serializer_class = BemorSerializer
-    lookup_field = 'uid'
-    permission_classes = []  # Open for everyone (no authentication required)
-
-    def retrieve(self, request, *args, **kwargs):
-        bemor = self.get_object()  # Get the specific Bemor instance
-
-        # Create a new Word document
-        doc = Document()
-        doc.add_heading("Bemor Ma'lumotlari", level=1)
-
-        # doc.add_paragraph(f"🆔 ID: {bemor.uid}")
-        doc.add_paragraph(f" Ism: {bemor.name}")
-        doc.add_paragraph(f"Kasallik: {bemor.kasallik}")
-        doc.add_paragraph(f" Tashxis: {bemor.tashxis}")
-        doc.add_paragraph(f"Shifokor: {bemor.doctor}")
-
-        # Prepare the HTTP response
-        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
-        response['Content-Disposition'] = f'attachment; filename=Bemor_{bemor.uid}.docx'
-
-        # Save the document to response
-        doc.save(response)
-        return response
-
-
 
 class BemorSearchView(generics.ListAPIView):
     queryset = Bemor.objects.all()
@@ -96,3 +52,98 @@ class BemorSearchView(generics.ListAPIView):
     permission_classes = []  # Open for everyone
     filter_backends = [filters.SearchFilter]
     search_fields = ['name']
+
+
+
+
+
+class BemorRetrieveView(generics.RetrieveAPIView):
+    queryset = Bemor.objects.all()
+    serializer_class = BemorSerializer
+    lookup_field = 'uid'
+
+    def get(self, request, *args, **kwargs):
+        bemor = self.get_object()
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = f'attachment; filename=Bemor_{bemor.uid}.pdf'
+
+        doc = SimpleDocTemplate(response, pagesize=A4,
+                                rightMargin=2*cm, leftMargin=2*cm,
+                                topMargin=2*cm, bottomMargin=2*cm)
+
+        styles = getSampleStyleSheet()
+        styles.add(ParagraphStyle(name='Heading', fontSize=16, alignment=TA_CENTER, spaceAfter=20))
+        styles.add(ParagraphStyle(name='Section', fontSize=12, spaceAfter=10, leading=15))
+        styles.add(ParagraphStyle(name='SubHeading', fontSize=13, leading=16, spaceBefore=12, spaceAfter=6,
+                                  fontName='Helvetica-Bold'))
+
+        elements = []
+
+        elements.append(Paragraph("Respublika ixtisoslashtirilgan urologiya "
+                                  
+                                  "ilmiy-amaliy tibbiyot markazi Farg\'ona filiali  ", styles['Heading']))
+        elements.append(Paragraph(f"Konsultativ poliklinika № 147966", styles['SubHeading']))
+
+        elements.append(Paragraph(f"<b>Ism:</b> {bemor.name}", styles['Section']))
+        elements.append(Paragraph(f"<b>Sana:</b> {bemor.sana.strftime('%Y-%m-%d %H:%M')}", styles['Section']))
+        elements.append(Paragraph(f"<b>Tug\'ilgan Sana:</b> {bemor.tugilgan.strftime('%Y-%m-%d %H:%M')}", styles['Section']))
+        elements.append(Paragraph(f"<b>Kasallik:</b> {bemor.kasallik}", styles['Section']))
+        elements.append(Paragraph(f"<b>Anamnezis morbi:</b> {bemor.anamnesis_m}", styles['Section']))
+        elements.append(Paragraph(f"<b>Anamnezis vitae:</b> {bemor.anamnesis_v}", styles['Section']))
+        elements.append(Paragraph(f"<b>status praesens:</b> {bemor.praesens}", styles['Section']))
+        elements.append(Paragraph(f"<b>Tekshiruv:</b> {bemor.tekshiruv}", styles['Section']))
+        elements.append(Paragraph(f"<b>Tashxis:</b> {bemor.tashxis}", styles['Section']))
+        elements.append(Paragraph(f"<b>Tavsiya:</b> {bemor.tavsiya}", styles['Section']))
+        elements.append(Paragraph(f"<b>Shifokor:</b> {bemor.doctor}", styles['Section']))
+        elements.append(Paragraph(f"<b>Mudir:</b> {bemor.mudir}", styles['Section']))
+
+        doc.build(elements)
+        return response
+
+
+class BemorDocxDownloadView(generics.RetrieveAPIView):
+    queryset = Bemor.objects.all()
+    serializer_class = BemorSerializer
+    lookup_field = 'uid'
+    permission_classes = []  # Open access
+
+    def retrieve(self, request, *args, **kwargs):
+        bemor = self.get_object()
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = f'attachment; filename=Bemor_{bemor.uid}.pdf'
+
+        doc = SimpleDocTemplate(response, pagesize=A4,
+                                rightMargin=2*cm, leftMargin=2*cm,
+                                topMargin=2*cm, bottomMargin=2*cm)
+
+        styles = getSampleStyleSheet()
+        styles.add(ParagraphStyle(name='Heading', fontSize=16, alignment=TA_CENTER, spaceAfter=20))
+        styles.add(ParagraphStyle(name='Section', fontSize=12, spaceAfter=10, leading=15))
+        styles.add(ParagraphStyle(name='SubHeading', fontSize=13, leading=16, spaceBefore=12, spaceAfter=6,
+                                  fontName='Helvetica-Bold'))
+
+        elements = []
+
+        elements.append(Paragraph("Respublika ixtisoslashtirilgan urologiya "
+                                  
+                                  "ilmiy-amaliy tibbiyot markazi Farg\'ona filiali  ", styles['Heading']))
+
+        elements.append(Paragraph(f"Konsultativ poliklinika № 147966", styles['SubHeading']))
+
+        elements.append(Paragraph(f"<b>Ism:</b> {bemor.name}", styles['Section']))
+        elements.append(Paragraph(f"<b>Sana:</b> {bemor.sana.strftime('%Y-%m-%d %H:%M')}", styles['Section']))
+        elements.append(Paragraph(f"<b>Tug\'ilgan Sana:</b> {bemor.tugilgan.strftime('%Y-%m-%d %H:%M')}", styles['Section']))
+        elements.append(Paragraph(f"<b>Kasallik:</b> {bemor.kasallik}", styles['Section']))
+        elements.append(Paragraph(f"<b>Anamnezis morbi:</b> {bemor.anamnesis_m}", styles['Section']))
+        elements.append(Paragraph(f"<b>Anamnezis vitae:</b> {bemor.anamnesis_v}", styles['Section']))
+        elements.append(Paragraph(f"<b>Praesens:</b> {bemor.praesens}", styles['Section']))
+        elements.append(Paragraph(f"<b>Tekshiruv:</b> {bemor.tekshiruv}", styles['Section']))
+        elements.append(Paragraph(f"<b>Tashxis:</b> {bemor.tashxis}", styles['Section']))
+        elements.append(Paragraph(f"<b>Tavsiya:</b> {bemor.tavsiya}", styles['Section']))
+        elements.append(Paragraph(f"<b>Shifokor:</b> {bemor.doctor}", styles['Section']))
+        elements.append(Paragraph(f"<b>Mudir:</b> {bemor.mudir}", styles['Section']))
+
+        doc.build(elements)
+        return response
+
+
